7d3a603730a55edf816e76267a4dc6238ca27465
Ludovic <34213508+B4L00@users.noreply.github.com>
2024-05-07 12:57:08 +0100

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,7 @@
+service mariadb start;
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mariadb -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mariadb -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+#mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+#exec mysqld_safe
